741e8036ec270e33d78463b49a0ef23290db462d
hailemaryam <hailemaryammecca@gmail.com>
2023-03-19 15:20:55 +0300

    DTO refactored using lombok

--- src/main/java/com/example/demo/DTO/DetectedMobileStation.java
+++ src/main/java/com/example/demo/DTO/DetectedMobileStation.java
@@ -6,41 +6,10 @@ import lombok.Data;
 
 import java.util.Date;
 
+@Data
+@AllArgsConstructor
 public class DetectedMobileStation {
     private String mobile_station_id;
     private float distance;
     private Date timestamp;
-
-    public DetectedMobileStation() {
-    }
-
-    public DetectedMobileStation(String mobile_station_id, float distance, Date timestamp) {
-        this.mobile_station_id = mobile_station_id;
-        this.distance = distance;
-        this.timestamp = timestamp;
-    }
-
-    public String getMobile_station_id() {
-        return mobile_station_id;
-    }
-
-    public void setMobile_station_id(String mobile_station_id) {
-        this.mobile_station_id = mobile_station_id;
-    }
-
-    public float getDistance() {
-        return distance;
-    }
-
-    public void setDistance(float distance) {
-        this.distance = distance;
-    }
-
-    public Date getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Date timestamp) {
-        this.timestamp = timestamp;
-    }
 }
