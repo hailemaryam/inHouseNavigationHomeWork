721b73bf8bc495cae06fa7f36ecc06117dd00b85
hailemaryam <hailemaryammecca@gmail.com>
2023-03-19 14:40:56 +0300

    distance formula used for detecting distance between two point

--- src/main/java/com/example/demo/util/CalculateDetectionDistance.java
+++ src/main/java/com/example/demo/util/CalculateDetectionDistance.java
@@ -4,7 +4,11 @@ import com.example.demo.domain.BaseStation;
 import com.example.demo.domain.MobileStation;
 
 public class CalculateDetectionDistance {
+
+//    distance formula used to calculate distance between mobile station and base station
     public static Float calculate(BaseStation baseStation, MobileStation mobileStation){
-        return new Float(10);
+        Float horizontalChange = baseStation.getX() - mobileStation.getLastKnownX();
+        Float verticalChange = baseStation.getY() - mobileStation.getLastKnownY();
+        return new Float(Math.sqrt(Math.pow(horizontalChange, 2) + Math.pow(verticalChange, 2)));
     }
 }
