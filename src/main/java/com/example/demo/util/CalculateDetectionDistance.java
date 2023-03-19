5143f6821201e1bb9b43993bfb53be1730e0b90b
hailemaryam <hailemaryammecca@gmail.com>
2023-03-19 14:24:51 +0300

    Detection distact  calculator added without implementation

---
+++ src/main/java/com/example/demo/util/CalculateDetectionDistance.java
@@ -0,0 +1,10 @@
+package com.example.demo.util;
+
+import com.example.demo.domain.BaseStation;
+import com.example.demo.domain.MobileStation;
+
+public class CalculateDetectionDistance {
+    public static Float calculate(BaseStation baseStation, MobileStation mobileStation){
+        return new Float(10);
+    }
+}
